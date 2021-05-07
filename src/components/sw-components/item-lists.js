840d789bef6640d23a1ff9989ae11fdf5d5352d6
Alexander Morozov <alexsvar@icloud.com>
2021-05-07 17:08:38 +0300

    Implemented the props transformation in higher order components

--- src/components/sw-components/item-lists.js
+++ src/components/sw-components/item-lists.js
@@ -1,13 +1,8 @@
 import React from 'react'
 
 import ItemList from '../item-list'
-import {withData} from '../hoc-helpers'
-import SwapiService from '../../services/swapi-service'
+import {withData, withSwapiService} from '../hoc-helpers'
 
-const swapiService = new SwapiService()
-const {
-  getAllPeople, getAllStarships, getAllPlanets
-} = swapiService
 
 const withChildFunction = (Wrapped, fn) => {
   return (props) => {
@@ -19,20 +14,47 @@ const withChildFunction = (Wrapped, fn) => {
   }
 }
 
+
+const mapPersonMethodsToProps = (swapiService) => {
+  return {
+    getData: swapiService.getAllPeople
+  }
+}
+
+const mapPlanetMethodsToProps = (swapiService) => {
+  return {
+    getData: swapiService.getAllPlanets
+  }
+}
+
+const mapStarshipMethodsToProps = (swapiService) => {
+  return {
+    getData: swapiService.getAllStarships
+  }
+}
+
 const renderName = ({name}) => <span>{name}</span>
 const renderModelAndName = ({model, name}) => <span>{name} ({model})</span>
 
-const PersonList = withData(
-  withChildFunction(ItemList, renderName),
-  getAllPeople
+const PersonList = withSwapiService(
+  withData(
+    withChildFunction(ItemList, renderName)
+  ),
+  mapPersonMethodsToProps
 )
-const StarshipList = withData(
-  withChildFunction(ItemList, renderModelAndName),
-  getAllStarships
+
+const PlanetList = withSwapiService(
+  withData(
+    withChildFunction(ItemList, renderName)
+  ),
+  mapPlanetMethodsToProps
 )
-const PlanetList = withData(
-  withChildFunction(ItemList, renderName),
-  getAllPlanets
+
+const StarshipList = withSwapiService(
+  withData(
+    withChildFunction(ItemList, renderModelAndName)
+  ),
+  mapStarshipMethodsToProps
 )
 
 export {
