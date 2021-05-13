3e53bdc849de49904f51847100d516c72a211737
Alexander Morozov <alexsvar@icloud.com>
2021-05-13 07:51:20 +0300

    Added the compose function

--- src/components/sw-components/item-lists.js
+++ src/components/sw-components/item-lists.js
@@ -1,18 +1,12 @@
 import React from 'react'
 
 import ItemList from '../item-list'
-import {withData, withSwapiService} from '../hoc-helpers'
-
-
-const withChildFunction = (fn) => (Wrapped) => {
-  return (props) => {
-    return (
-      <Wrapped {...props}>
-        {fn}
-      </Wrapped>
-    )
-  }
-}
+import {
+  withData,
+  withSwapiService,
+  withChildFunction,
+  compose
+} from '../hoc-helpers'
 
 
 const mapPersonMethodsToProps = (swapiService) => {
@@ -36,20 +30,23 @@ const mapStarshipMethodsToProps = (swapiService) => {
 const renderName = ({name}) => <span>{name}</span>
 const renderModelAndName = ({model, name}) => <span>{name} ({model})</span>
 
-const PersonList = withSwapiService(mapPersonMethodsToProps)(
-  withData(
-    withChildFunction(renderName)(
-      ItemList)))
+const PersonList = compose(
+  withSwapiService(mapPersonMethodsToProps),
+  withData,
+  withChildFunction(renderName)
+)(ItemList)
 
-const PlanetList = withSwapiService(mapPlanetMethodsToProps)(
-  withData(
-    withChildFunction(renderName)(
-      ItemList)))
+const PlanetList = compose(
+  withSwapiService(mapPlanetMethodsToProps),
+  withData,
+  withChildFunction(renderName)
+)(ItemList)
 
-const StarshipList = withSwapiService(mapStarshipMethodsToProps)(
-  withData(
-    withChildFunction(renderModelAndName)(
-      ItemList)))
+const StarshipList = compose(
+  withSwapiService(mapStarshipMethodsToProps),
+  withData,
+  withChildFunction(renderModelAndName)
+)(ItemList)
 
 
 export {
