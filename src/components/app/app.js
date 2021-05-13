472cfd6f6382a7705e1d31c0462648b0ea53da16
Alexander Morozov <alexsvar@icloud.com>
2021-05-13 09:13:36 +0300

    Installed react-router-dom library, added routes

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -8,8 +8,9 @@ import DummySwapiService from '../../services/dummy-swapi-service'
 import {SwapiServiceProvider} from '../swapi-service-context'
 import {PeoplePage, PlanetsPage, StarshipsPage} from '../pages/'
 
-
 import './app.css'
+
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 
 export default class App extends Component {
 
@@ -36,15 +37,18 @@ export default class App extends Component {
     return (
       <ErrorBoundry>
         <SwapiServiceProvider value={this.state.swapiService}>
-          <div className="stardb-app">
+          <Router>
+            <div className="stardb-app">
 
-            <Header onServiceChange={this.onServiceChange}/>
-            <RandomPlanet/>
-            <PeoplePage/>
-            <PlanetsPage/>
-            <StarshipsPage/>
+              <Header onServiceChange={this.onServiceChange}/>
+              <RandomPlanet/>
 
-          </div>
+              <Route path="/people" component={PeoplePage}/>
+              <Route path="/planets" component={PlanetsPage}/>
+              <Route path="/starships" component={StarshipsPage}/>
+
+            </div>
+          </Router>
         </SwapiServiceProvider>
       </ErrorBoundry>
     )
