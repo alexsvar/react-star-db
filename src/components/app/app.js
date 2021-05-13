f72ba4d3db72755494595ab74ab54f0a2d3faf48
Alexander Morozov <alexsvar@icloud.com>
2021-05-13 10:36:08 +0300

    Added relative paths

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -53,7 +53,8 @@ export default class App extends Component {
                 render={() => <h2>Welcome to StarDB</h2>}
                 exact
               />
-              <Route path="/people" component={PeoplePage}/>
+              <Route path="/people" render={() => <h2>People</h2>} exact/>
+              <Route path="/people/:id?" component={PeoplePage}/>
 
               <Route path="/planets" component={PlanetsPage}/>
 
